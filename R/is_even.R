5bedb3b473685c2c6e30b4f0304b23abe31b0eeb
Kyle Harris <koderkow@gmail.com>
2019-05-09 03:16:03 -0400

    fixed warnings

--- R/is_even.R
+++ R/is_even.R
@@ -1,3 +1,13 @@
+#' Is this value even?
+#'
+#' Reveals if x is even, where x is a numeric value
+#'
+#' @param x numeric value
+#'
+#' @return boolean
+#' @export
+#'
+#' @examples
 is_even <- function(x) {
   x %% 2 == 0
 }
